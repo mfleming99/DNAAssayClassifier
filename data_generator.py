a8593d9581b42f37f6662bc12ba340d674a7d091
Maxwell Fleming <mjfleming99@gmail.com>
2019-09-26 22:11:57 -0400

    Created a frequency tree for gene annotation %

--- data_generator.py
+++ data_generator.py
@@ -1,5 +1,6 @@
 from parser import parseFile
 from ncls import NCLS
+from collections import defaultdict
 import sys
 import subprocess
 import csv
@@ -10,48 +11,55 @@ reads_per_random_index = 5
 
 def main(cvs_file, gtf_file):
 
-    print("############# STARTING #############", file=sys.stderr)
-
-    frequency_tree = handle_gtf(gtf_file)
+    print("############# STARTING", file=sys.stderr)
+    print("############# STARTING CONSTURCTING FREQUENCY TREE", file=sys.stderr)
+    frequency_trees = handle_gtf(gtf_file)
+    print("############# ENDING CONSTURCTING FREQUENCY TREE", file=sys.stderr)
+    print("############# STARTING HANDLEING CSV", file=sys.stderr)
     data = handle_csv(cvs_file)
-    run_bowtie(data)
-
-    print("############# FINSHING #############", file=sys.stderr)
-
+    print("############# FINSHING HANDLEING CSV", file=sys.stderr)
+    print("############# BEGINING SEQUENCING", file=sys.stderr)
+    run_bowtie(data, frequency_trees)
+    print("############# FINISHED SEQUENCING", file=sys.stderr)
+    print("############# FINSHING", file=sys.stderr)
     with open("data_output.csv","w+") as my_csv:
         csvWriter = csv.writer(my_csv,delimiter=',')
         csvWriter.writerows(data)
 
 
 def handle_csv(file):
-
-    print("############# STARTING HANDLEING CSV #############", file=sys.stderr)
-
     datafile = open(file, 'r')
     datareader = csv.reader(datafile, delimiter=',')
     data = []
     for row in datareader:
         data.append(row)
-
-    print("############# FINSHING HANDLEING CSV #############", file=sys.stderr)
-
     return data
 
+def generate_empty_list():
+    list = []
+    list.append([])
+    list.append([])
+    return list
+
 def handle_gtf(file):
-    starts = np.array()
-    ends = np.array()
+    frequency_trees = defaultdict(generate_empty_list)
     gtf = open(file, 'r')
     for line in gtf:
-        line = line.split(' ')
-        starts.append([int(line[3])])
-        ends.append([int(line[4])])
-    ids = starts
-    ncls = NCLS(starts, ends, ids)
+        line = line.split('\t')
+        try:
+            chr = int(line[0])
+            try:
+                frequency_trees[chr][0].append(int(line[3]))
+                frequency_trees[chr][1].append(int(line[4]))
+            except:
+                print("Lol we have a real problem")
+        except:
+            print("Chromosome was non-integer")
+    for key in frequency_trees.keys():
+        frequency_trees[key] = NCLS(np.array(frequency_trees[key][0]), np.array(frequency_trees[key][1]), np.array(frequency_trees[key][0]))
+    return frequency_trees
 
-def run_bowtie(contents):
-
-    print("############# BEGINING SEQUENCING #############", file=sys.stderr)
-
+def run_bowtie(contents, frequency_tree):
     for i in range(len(contents)):
 
         print("############# BEGINING SEQUENCING " + str(i + 1) + " OF " + str(len(contents)) + " #############", file=sys.stderr)
@@ -66,8 +74,6 @@ def run_bowtie(contents):
         subprocess.call("rm temp.sam")
         for value in data:
             contents[i].append(value)
-            
-    print("############# FINISHED SEQUENCING #############", file=sys.stderr)
 
 def get_spots(sra_label):
     print(sra_label)
