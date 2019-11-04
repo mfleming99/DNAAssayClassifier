4c3b24e18159092c29962ea90d139439715d8a44
Maxwell Fleming <mfleming@Maxwells-MacBook-Pro.local>
2019-11-04 17:57:05 -0500

    Test

--- data_generator.py
+++ data_generator.py
@@ -47,7 +47,8 @@ def run_bowtie(bowtie_index, contents, frequency_tree):
     outputs = []
     for i in range(len(contents)):
         print("############# BEGINING SEQUENCING " + str(i + 1) + " OF " + str(len(contents)) + " #############", file = sys.stderr)
-        subprocess.call(["/software/bowtie2/bowtie2-align-s", "-x", bowtie_index, "--sra-acc", contents[i][0], "-sample-sra", str(reads_to_be_analized) , "--treads", "4", ">> $HOME/temp.sam"], shell=True)
+        print(bowtie_index)
+        subprocess.call(["/software/bowtie2/bowtie2-align-s", "-x", bowtie_index, "--sra-acc", contents[i][0], "-sample-sra", str(reads_to_be_analized) , "--treads", "4", ">>", "$HOME/temp.sam"], shell=True)
         print("############# FINISHED SEQUENCING " + str(i + 1) + " OF " + str(len(contents)) + " #############", file = sys.stderr)
         data = parseFile("$HOME/temp.sam", frequency_tree)
         subprocess.call(["rm temp.sam"], shell=True)
