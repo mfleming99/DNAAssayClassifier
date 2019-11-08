3efecd9ec0ecd5a3e4c6472391e0efb92d4d110f
Maxwell Fleming <mfleming@Maxwells-MacBook-Pro.local>
2019-11-08 11:34:03 -0500

    No merge errors

---
+++ data/data_generator.py
@@ -0,0 +1,70 @@
+from parser import parseFile #COMMENT THIS LINE OUT IF YOU WANT TO RUN PARSER AS MAIN
+from ncls import NCLS
+from collections import defaultdict
+import sys
+import subprocess
+import csv
+import random
+import numpy as np
+import shlex
+
+def handle_csv(file):
+    datafile = open(file, 'r')
+    datareader = csv.reader(datafile, delimiter=',')
+    data = []
+    for row in datareader:
+        data.append(row)
+    return data
+
+def generate_empty_list():
+    list = []
+    list.append([])
+    list.append([])
+    return list
+
+def handle_gtf(file):
+    frequency_trees = defaultdict(generate_empty_list)
+    gtf = open(file, 'r')
+    for line in gtf:
+        line = line.split('\t')
+        try:
+            chr = int(line[0])
+            try:
+                if (line[2] != 'transcript'):
+                    frequency_trees[chr][0].append(int(line[3]))
+                    frequency_trees[chr][1].append(int(line[4]))
+            except:
+                print("This shouldn't happen - GTF handleing is wrong")
+        except:
+            pass
+            #print("Chromosome was non-integer")
+    for key in frequency_trees.keys():
+        frequency_trees[key] = NCLS(np.array(frequency_trees[key][0]), np.array(frequency_trees[key][1]), np.array(frequency_trees[key][0]))
+    return frequency_trees
+
+def run_bowtie(bowtie_index, contents, frequency_tree):
+    reads_to_be_analized = 10000
+    reads_per_random_index = 50
+    outputs = []
+    for i in range(len(contents)):
+        print("############# BEGINING SEQUENCING " + str(i + 1) + " OF " + str(len(contents)) + " #############", file = sys.stderr)
+#         args = shlex.split("/software/bowtie2/bowtie2 -x /root/indexes/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna.bowtie_index --sra-acc " + contents[i][1] + " -sample-sra " + str(reads_to_be_analized) + " --threads 4 >> /root/temp.sam")
+#         print("ARGS = " + str(args))
+#         subprocess.call(args,  shell=True)
+        args = shlex.split("/software/bowtie2/bowtie2 -x /root/indexes/GCA_000001405.15_GRCh38_no_alt_analysis_set.fna.bowtie_index --sra-acc " + contents[i][1])
+        subprocess.call(args,  shell=True)
+        # subprocess.call(["/software/bowtie2/bowtie2 -x " + bowtie_index + " --sra-acc " + contents[i][0] + " -sample-sra " + str(reads_to_be_analized) + " -- threads 4 >> /root/temp.sam"],  shell=True)
+        # subprocess.call(["/software/bowtie2/bowtie2", "-x", bowtie_index, "--sra-acc", contents[i][0], "-sample-sra", str(reads_to_be_analized) , "--treads", "4", ">>", "/root/temp.sam"], shell=True)
+        print("############# FINISHED SEQUENCING " + str(i + 1) + " OF " + str(len(contents)) + " #############", file = sys.stderr)
+        data = parseFile("/root/temp.sam", frequency_tree)
+        args = shlex.split("rm temp.sam")
+        subprocess.call(args, shell=True)
+        outputs.append(data)
+        for value in data:
+            contents[i].append(value)
+    return outputs
+
+# def get_spots(sra_label):
+#     print(sra_label)
+#     query_result = subprocess.check_output("esearch -db sra -query " +  sra_label  +  " | efetch -format runinfo", shell=True)
+#     return int((query_result.decode().split('\n')[1]).split(',')[3])
---
+++ data/parser.py
@@ -0,0 +1,86 @@
+from collections import defaultdict
+import numpy as np
+import sys
+import re
+
+def parseFile(file, frequency_tree):
+    print("############# OPENING SAM FILE", file=sys.stderr)
+    with open(file, 'rt') as myfile:
+        contents = myfile.read()
+    return parseString(contents, frequency_tree)
+
+
+def parseString(txt, frequency_tree):
+    spliter = re.compile('\n+')
+    readnumber = re.compile('[r]+\d+')
+    line_spliter = re.compile('\t+')
+    colon_spliter = re.compile(':')
+    forward_reads = 0
+    reverse_reads = 0
+    unmatched_reads = 0
+    read_positions = defaultdict(list)
+    position_differences = []
+    position_differences_stdv_list = []
+    total_position_diffs = []
+    read_lengths_count = 0
+    read_lengths_total = 0
+    read_frequency = 0
+    read_lengths_average = 0
+    num_chromosomes = 0
+
+    lines = spliter.split(txt)
+    #Itterating though everyline
+    for i in range(len(lines) - 1):
+        subline = line_spliter.split(lines[i])
+        if (int(subline[1]) & 4 == 4):
+            unmatched_reads += 1
+        elif (int(subline[1]) & 16 == 16):
+            reverse_reads += 1
+        else:
+            forward_reads += 1
+        read = subline[9]
+        read_lengths_count += 1
+        read_lengths_total += len(read)
+        chromosome = getChromosome(subline[2])
+        if chromosome != -1:
+            read_positions[chromosome].append(int(subline[3]))
+    if read_lengths_count != 0:
+        read_lengths_average = read_lengths_total / read_lengths_count
+    if (forward_reads + reverse_reads + unmatched_reads) != 0:
+        read_frequency = (forward_reads + reverse_reads) / (forward_reads + reverse_reads + unmatched_reads)
+
+    gene_annotation_match = 0
+    gene_annotation_total = 0
+    gene_annotation_percent = 0
+    for key in read_positions.keys():
+        for position in read_positions[key]:
+            #TODO there is for sure a better way to do this than with a break
+            for _ in frequency_tree[key].find_overlap(position, position):
+                gene_annotation_match += 1
+                break
+            gene_annotation_total += 1
+    if gene_annotation_total != 0:
+        gene_annotation_percent = gene_annotation_match / gene_annotation_total
+    print("gene_annotation_percent = " + str(gene_annotation_percent))
+
+    for _, position_list in read_positions.items():
+        position_list.sort()
+        num_chromosomes += 1
+        for i in range(len(position_list) - 1):
+            position_differences.append(position_list[i + 1] - position_list[i])
+    try:
+        std_of_pos_diff = np.std(position_differences)
+        mean_of_pos_diffs = np.nanmean(position_differences)
+        max_position_difference = np.amax(position_differences)
+        min_position_difference = np.amin(position_differences)
+    except:
+        return None
+    return [gene_annotation_percent, read_lengths_average, read_frequency, std_of_pos_diff, mean_of_pos_diffs, num_chromosomes, max_position_difference, min_position_difference]
+
+def getChromosome(str):
+    if str == "*" or str[3:] == 'X':
+        return -1
+    try:
+        return int(str[3:])
+    except:
+        return -1
